fbbe971a6a9b044e320a09f6e61d5b9483d44a0c
Mariana <marianacavalle1@gmail.com>
2023-08-27 22:19:56 -0300

    classe exemplo smartTv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
+
+        smartTv.mudarCanal(501);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+    }
+}
